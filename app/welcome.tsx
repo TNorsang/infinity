6393a62330e3cbc19140138d6069a40d21c59836
TNorsang <thenorsang@gmail.com>
2024-11-06 17:20:11 -0500

    updated welcome page

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -1,30 +1,40 @@
 import { StyleSheet, Text, Image, View } from "react-native";
 import React from "react";
-import ScreenWrapper from "../components/ScreenWrapper";
 import { StatusBar } from "expo-status-bar";
 import { hp, wp } from "../helpers/common";
 import { theme } from "../constants/theme";
+import { LinearGradient } from "expo-linear-gradient";
 
 const welcome = () => {
   return (
-    <ScreenWrapper bg="white">
-      <StatusBar style="dark" />
-      <View className="bg-black">
-        <Text className="text-white">testasdf</Text>{" "}
-      </View>
-      <View style={styles.container}>
+    <View style={styles.container}>
+      {/* Upper half without gradient */}
+      <View style={styles.upperHalf} className="z-20">
         <Image
           style={styles.welcomeImage}
           resizeMode="contain"
-          source={require("../assets/images/logo.png")}
+          source={require("../assets/images/family_transparent.png")}
         />
         {/* Title */}
-        <View style={{ gap: 20 }}>
-          <Text style={styles.title}>Infinity</Text>
-          <Text style={styles.punchline}>The Infinity App</Text>
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>infinity</Text>
+          <Text style={styles.punchline}>
+            Family stays close, no matter how far apart
+          </Text>
         </View>
       </View>
-    </ScreenWrapper>
+
+      {/* Bottom half with gradient */}
+      <LinearGradient
+        className="z-10"
+        colors={[
+          "rgba(255, 246, 229, 0)", // Light orange with 0% opacity (transparent)
+          "rgba(255, 209, 209, 0.6)", // Light pink with 60% opacity
+          "rgba(255, 246, 229, 1)", // Light orange with 100% opacity
+        ]}
+        style={styles.gradient}
+      ></LinearGradient>
+    </View>
   );
 };
 
@@ -33,24 +43,43 @@ export default welcome;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "white",
+  },
+  upperHalf: {
+    flex: 1,
     alignItems: "center",
-    justifyContent: "space-around",
-    marginHorizontal: wp(10),
+    justifyContent: "center",
+  },
+  gradient: {
+    height: "70%", // Cover the bottom half of the screen
+    width: "500%",
+    position: "absolute",
+    bottom: 0, // Position it at the bottom of the screen
   },
   welcomeImage: {
-    height: hp(15),
+    height: hp(30),
+    width: hp(30),
     alignSelf: "center",
+  },
+  textContainer: {
+    marginTop: 20,
+    alignItems: "center",
   },
   title: {
     color: theme.colors.text,
+    position: "relative",
+    bottom: 50,
+    marginTop: hp(20),
     fontSize: hp(4),
     textAlign: "center",
     fontWeight: theme.fonts.extraBold as "800",
   },
   punchline: {
     textAlign: "center",
-    paddingHorizontal: wp(10),
-    fontSize: hp(1.7),
+    position: "relative",
+    bottom: 50,
+    marginTop: hp(2),
+    fontSize: hp(2),
     color: theme.colors.text,
   },
 });
