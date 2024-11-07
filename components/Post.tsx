221a7416d1222d592f428fa243bbcc5bf1c89291
TNorsang <thenorsang@gmail.com>
2024-11-07 18:24:20 -0500

    Updated Feed Page with images

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Platform, Image } from "react-native";
 import React from "react";
 import { wp, hp } from "../helpers/common";
 
@@ -12,7 +12,13 @@ const Post: React.FC<postProps> = (props) => {
       <View style={styles.accountContainer}>
         <Text style={styles.account}>{props.account}</Text>
       </View>
-      <Text style={styles.text}>{props.content}</Text>
+      <View>
+        <Image
+          resizeMode="contain"
+          source={require("@/assets/images/post_image_one.png")}
+        />
+        <Text style={styles.text}>{props.content}</Text>
+      </View>
     </View>
   );
 };
@@ -21,12 +27,11 @@ export default Post;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "orange",
+    backgroundColor: "white",
     width: wp(80),
     height: hp(30),
     marginVertical: hp(5),
     marginHorizontal: wp(4),
-    alignItems: "center",
     borderRadius: 30,
     padding: 10,
   },
@@ -34,13 +39,13 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   account: {
-    color: "white", // Text color for the account name
+    color: "black", // Text color for the account name
     fontWeight: "800",
-    fontSize: wp(4), // Adjust font size if needed
+    fontSize: Platform.OS === "web" ? wp(1.5) : wp(5.5),
   },
   text: {
     color: "gray",
-    fontWeight: "600",
-    fontSize: wp(2),
+    fontWeight: "400",
+    fontSize: wp(1),
   },
 });
